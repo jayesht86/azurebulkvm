f572477a28a9a33cc0ecde3de8bb9a91583de92b
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-02-21 14:26:01 +0530

    Update variables.tf

--- module/nic/variables.tf
+++ module/nic/variables.tf
@@ -13,3 +13,13 @@ variable "subnet_id" {
 variable "vm_name" {
   type = string
 }
+variable "network_interfaces" {
+  type = list(object({
+    name                          = string
+    enable_accelerated_networking = optional(bool, false)
+    nic_ip_config = optional(list(object({
+      name                          = string
+      private_ip_address_allocation = optional(string, "Dynamic")
+    })), [])
+  }))
+}
