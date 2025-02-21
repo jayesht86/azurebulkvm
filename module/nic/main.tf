72cc8d92234342f4479054b19908f9cdd2bd855e
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-02-21 14:25:24 +0530

    Update main.tf

--- module/nic/main.tf
+++ module/nic/main.tf
@@ -1,11 +1,25 @@
 resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic2"
+  name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "Dynamic"
+  enable_accelerated_networking = length(var.network_interfaces) > 0 ? var.network_interfaces[0].enable_accelerated_networking : false
+
+  dynamic "ip_configuration" {
+    for_each = { for idx, ip_config in flatten([
+      for nic in var.network_interfaces : (
+        length(lookup(nic, "nic_ip_config", [])) > 0 ? nic.nic_ip_config : [{
+          name                          = "ipconfig-default"
+          private_ip_address_allocation = "Dynamic"
+        }]
+      )
+    ]) : idx => ip_config }
+
+    content {
+      name                          = ip_configuration.value.name
+      subnet_id                     = data.azurerm_subnet.subnet.id
+      private_ip_address_allocation = ip_configuration.value.private_ip_address_allocation
+      private_ip_address            = lookup(ip_configuration.value, "private_ip_address", null)
+    }
   }
 }
